08293e1a1aa451e7fac7462f6841a1d3bc58fe66
rayvikram <vik8876@gmail.com>
2020-09-14 17:23:06 +0530

    Store and css added

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import listingReducer from "../features/Listings/ListingsSlice"
 
 export default configureStore({
   reducer: {
-    counter: counterReducer,
+    listing: listingReducer,
   },
 });
