b2565daa1794d1ee04ba876a5cb7433df2f44c19
rayvikram <vik8876@gmail.com>
2020-09-14 17:35:00 +0530

    Updates

---
+++ precache-manifest.821fc2b3bca3b54625c9e68ba4e31a37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ce50abd3944aa520d5a63a4ac9ae88a",
+    "url": "/product-listing/index.html"
+  },
+  {
+    "revision": "0be6e19b7e0d0fd498d6",
+    "url": "/product-listing/static/css/main.530fc570.chunk.css"
+  },
+  {
+    "revision": "34d7577f664564f563f5",
+    "url": "/product-listing/static/js/2.980dd07e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/product-listing/static/js/2.980dd07e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0be6e19b7e0d0fd498d6",
+    "url": "/product-listing/static/js/main.f3c506e6.chunk.js"
+  },
+  {
+    "revision": "68275dfab5e4132613e3",
+    "url": "/product-listing/static/js/runtime-main.deece3d5.js"
+  }
+]);
